54c364055defac7efec245ba935c5616c15da27e
Johnson Su <johnsonsu0603@gmail.com>
2017-04-12 21:56:28 +0800

    Added Android FinishedPlaying event

--- android/src/main/java/rnsoundplayer/RNSoundPlayerModule.java
+++ android/src/main/java/rnsoundplayer/RNSoundPlayerModule.java
@@ -1,14 +1,20 @@
-package hk.com.hcg.rnsoundplayer;
+package rnsoundplayer;
 
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
 
 import java.io.IOException;
+import javax.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 
 public class RNSoundPlayerModule extends ReactContextBaseJavaModule {
@@ -31,6 +37,15 @@ public class RNSoundPlayerModule extends ReactContextBaseJavaModule {
     if (this.mediaPlayer == null) {
       int soundResID = getReactApplicationContext().getResources().getIdentifier(name, "raw", getReactApplicationContext().getPackageName());
       this.mediaPlayer = MediaPlayer.create(getCurrentActivity(), soundResID);
+      this.mediaPlayer.setOnCompletionListener(
+        new OnCompletionListener() {
+          @Override
+          public void onCompletion(MediaPlayer arg0) {
+            WritableMap params = Arguments.createMap();
+            params.putBoolean("success", true);
+            sendEvent(getReactApplicationContext(), "FinishedPlaying", params);
+          }
+      });
     } else {
       Uri uri = Uri.parse("android.resource://" + getReactApplicationContext().getPackageName() + "/raw/" + name);
       this.mediaPlayer.reset();
@@ -38,6 +53,14 @@ public class RNSoundPlayerModule extends ReactContextBaseJavaModule {
       this.mediaPlayer.prepare();
     }
     this.mediaPlayer.start();
+  }
+
+  private void sendEvent(ReactApplicationContext reactContext,
+                       String eventName,
+                       @Nullable WritableMap params) {
+    reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, params);
   }
 
 }
--- android/src/main/java/rnsoundplayer/RNSoundPlayerPackage.java
+++ android/src/main/java/rnsoundplayer/RNSoundPlayerPackage.java
@@ -1,5 +1,4 @@
-
-package hk.com.hcg.rnsoundplayer;
+package rnsoundplayer;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,6 +9,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNSoundPlayerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
